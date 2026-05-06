baff9044e35d752c475c032800b59eebdf8a9e6a
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-05-06 13:36:49 +0200

    Add task 1

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for c in (str):
+        if ord(c) >= 97 and ord(c) <= 122:
+            print("{}".format(chr(ord(c) - 32)), end="")
+        else:
+            print("{}".format(c), end="")
+    print("")
+
+
+if __name__ == "__main__":
+    uppercase("Holberton")
