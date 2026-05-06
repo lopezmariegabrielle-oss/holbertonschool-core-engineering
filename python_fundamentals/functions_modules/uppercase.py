09958211d011b3b8ba448d2dab305761328bdeb3
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-05-06 13:42:53 +0200

    Add task 1

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -2,9 +2,9 @@
 def uppercase(str):
     for c in (str):
         if ord(c) >= 97 and ord(c) <= 122:
-            print("{}".format(chr(ord(c) - 32)), end="")
+            print(chr(ord(c) - 32), end="")
         else:
-            print("{}".format(c), end="")
+            print(c, end="")
     print("")
 
 
