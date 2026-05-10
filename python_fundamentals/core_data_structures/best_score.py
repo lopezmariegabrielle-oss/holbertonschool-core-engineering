b6720b65c46f3f42c41ae0a4baa13c22ebc3b036
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-05-10 15:15:36 +0200

    Add task 7

---
+++ python_fundamentals/core_data_structures/best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best_k = list(a_dictionary.keys())[0]
+    for key in a_dictionary:
+        if a_dictionary[key] > a_dictionary[best_k]:
+            best_k = key
+
+    return best_k
