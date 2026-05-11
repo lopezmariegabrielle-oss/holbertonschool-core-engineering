84fd3d13b349b5340a9345cdfff0d165f6a64f1f
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-05-11 13:52:21 +0200

    Add task 2

--- python_fundamentals/exception_handling/main.py
+++ python_fundamentals/exception_handling/main.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
-safe_print_list = __import__('safe_print_list').safe_print_list
+safe_print_list_integers = __import__('safe_print_list_integers').safe_print_list_integers
 
 my_list = [1, 2, 3, 4, 5]
 
-nb_print = safe_print_list(my_list, 2)
-print(f"elements: {nb_print}")
+nb_print = safe_print_list_integers(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+
+my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+nb_print = safe_print_list_integers(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+
+nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
