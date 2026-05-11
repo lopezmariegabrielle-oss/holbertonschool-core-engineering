cbe729ae98abf3bf057c904feb793b3758f8b5c0
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-05-11 15:13:40 +0200

    Add task 4

--- python_fundamentals/exception_handling/main.py
+++ python_fundamentals/exception_handling/main.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-safe_print_division = __import__('safe_print_division').safe_print_division
+raise_exception = __import__('raise_exception').raise_exception
 
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+try:
+    raise_exception()
+except TypeError:
+    print("Exception raised")
