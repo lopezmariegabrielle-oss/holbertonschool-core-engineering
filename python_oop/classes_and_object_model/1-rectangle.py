df7e672e3a82f7f5f0b4b64d0915efaae16484ef
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-05-12 16:38:37 +0200

    Add task 8

---
+++ python_oop/classes_and_object_model/1-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("widht must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an interger")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
