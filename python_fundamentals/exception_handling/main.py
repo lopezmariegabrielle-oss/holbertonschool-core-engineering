d871f09eaa97484e4c213f3e9db8266577b0b731
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-05-13 10:04:03 +0200

    Add task

--- python_fundamentals/exception_handling/main.py
+++ python_fundamentals/exception_handling/main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
-raise_exception = __import__('raise_exception').raise_exception
+raise_exception_msg = __import__('raise_exception_msg').raise_exception_msg
 
 try:
-    raise_exception()
-except TypeError:
-    print("Exception raised")
+    # On appelle ta fonction avec un message spécifique
+    raise_exception_msg("C'est un message de test !")
+except NameError as ne:
+    # On attrape l'erreur et on affiche le message qu'elle contient
+    print(ne)
+    
