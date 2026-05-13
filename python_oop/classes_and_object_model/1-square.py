aa6c048a45bc15cd62b4767f867b7d6efb987ce0
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-05-13 10:17:44 +0200

    Add task

--- python_oop/classes_and_object_model/1-square.py
+++ python_oop/classes_and_object_model/1-square.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+"""
+Module 1-square
+Définit une classe Square avec un attribut privé.
+"""
+
+
 class Square:
+    """
+    Classe Square qui définit un carré par sa taille.
+
+    Attributes:
+        __size (int): La taille d'un côté du carré (privée).
+    """
+
     def __init__(self, size):
+        """
+        Initialise une nouvelle instance de Square.
+
+        Args:
+            size (int): La taille du côté du nouveau carré.
+        """
         self.__size = size
