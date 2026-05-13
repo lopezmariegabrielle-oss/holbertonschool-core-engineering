7e2b3a50c0bf0bf0e047fbe4a01f3d88254c2f1b
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-05-13 10:10:34 +0200

    Add task

--- python_oop/classes_and_object_model/0-square.py
+++ python_oop/classes_and_object_model/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python3
+"""
+Ce module définit une classe Square vide.
+C'est le début de l'apprentissage de la POO.
+"""
+
 class Square:
+    """
+    Classe Square qui définit un carré.
+    Pour l'instant, elle ne contient ni attributs ni méthodes.
+    """
     pass
