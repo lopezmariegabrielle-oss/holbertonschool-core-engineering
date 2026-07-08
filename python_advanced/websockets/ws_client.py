0c45ac73876c6df5dfac72d4e73881df78e39b24
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-07-08 11:55:50 +0200

    Add task

---
+++ python_advanced/websockets/ws_client.py
@@ -0,0 +1,16 @@
+"""Module for a basic WebSocket client."""
+
+import asyncio
+import websockets
+
+
+async def hello():
+    """Connect to the server, send a message, and print the response."""
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        await websocket.send("Hello WebSocket")
+        response = await websocket.recv()
+        print(response)
+
+if __name__ == "__main__":
+    asyncio.run(hello())
